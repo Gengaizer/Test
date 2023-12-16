b6e5b69c3623f5370ff0f9fc07e169f9d5df7dd6
Gengaizer <146122346+Gengaizer@users.noreply.github.com>
2023-12-16 18:12:27 +0300

    Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿string[] strings = { "ни", "чего", "не","понятно", "но" , "очень", "интересно"};
+
+
+static int ArrayLength (string[] strings)
+{
+    int count = 0;
+    for (int i = 0; i < strings.Length; i++)
+    {
+       if (strings[i].Length<=3)
+        {
+            count++;
+        }
+       
+    }
+  return count;
+}
+
+string[] Count = new string[ArrayLength(strings)];
+
+void Array (string[] strings,string[] Count)
+{
+    int count = 0;
+    for(int i = 0; i < strings.Length; i++)
+    {
+        if(strings[i].Length <= 3)
+        {
+            Count[count] = strings[i];
+           
+            count++;
+
+        }
+    }
+}
+
+ArrayLength(strings);
+Array(strings, Count);
+Console.Write($"{string.Join(", ", Count)}");
